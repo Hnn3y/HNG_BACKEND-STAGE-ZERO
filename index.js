c775d4f545918c6cad6a4bc2e68a8f7770273448
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-01-29 20:07:41 +0100

    HNG TASK 0

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+
+
 const app = express();
+app.use(cors()); 
 
 app.get("/" , (req, res) => {
-    res.json({
-        email: "eenijeshiku@gmail.com",
-        current_dateTime: new Date().toISOString(),
+    const response = {
+        email:  "eenijeshiku@gmail.com",
+        current_datetime: new Date().toISOString(),
         github_url: "https://github.com/Hnn3y/HNG_BACKEND-STAGE-ZERO.git", 
-    }); 
+    }; 
+    res.json(response);
+});  
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
 }); 
